3f7dc3ed8ff0d6685f56d29d5730f0bf536e6b6e
Phil Sung <psung@dnanexus.com>
2017-04-19 16:40:55 -0700

    oops, forgot to invoke the method

--- src/python/dxpy/scripts/dx.py
+++ src/python/dxpy/scripts/dx.py
@@ -737,7 +737,7 @@ def ls(args):
             if args.brief:
                 describe_input = dict(fields={'id': True, 'name': True})
             elif args.verbose:
-                describe_input = dict(fields=get_ls_l_desc_fields)
+                describe_input = dict(fields=get_ls_l_desc_fields())
             else:
                 describe_input = dict(fields={'id': True, 'class': True, 'name': True})
             resp = dxproj.list_folder(folder=folderpath,
